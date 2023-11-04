2f3014aba8e308fab0cab06052c30451691009a8
Vishal Agrahari <vishalagraharibasti@gmail.com>
2023-11-05 02:26:46 +0530

    Added Routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Home from './Components/Home'
+import React from 'react';
+import Home from './Components/Home';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SingleMovie from './Components/SingleMovie';
+import Error from './Components/Error';
+
+
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-      
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="movie/:id" element={<SingleMovie/>}/>
+      <Route path="*" element={<Error/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
