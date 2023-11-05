45a51f18d8ced802640c342d89e3b72c34ee831a
Vishal Agrahari <vishalagraharibasti@gmail.com>
2023-11-05 11:39:20 +0530

    Added API_KEYs

--- src/Components/context.js
+++ src/Components/context.js
@@ -1,8 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+
+
+const API_URL='http://www.omdbapi.com/?apikey=5905a8d4&s=love';
 
 const AppContext=React.createContext();
 
 const AppProvider = ({children}) => {
+
+    const getMovie = async (url) =>{
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        getMovie(API_URL);
+    },[]);
+
+
     return <AppContext.Provider value="Vishal">{children}</AppContext.Provider>
 }
 
